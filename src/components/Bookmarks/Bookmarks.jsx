b303052334177e9f2a90139314777d7ff9e7bc3c
Munshi91 <mimunshi91@gmail.com>
2023-09-19 15:34:50 +0600

    Update Bookmarks.jsx

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types';
 import Bookmark from '../Bookmark/Bookmark';
 
-const Bookmarks = ({ bookmarks }) => {
+const Bookmarks = ({ bookmarks, readingTime }) => {
   return (
     <div className="md:w-1/3 ml-4 bg-gray-300 mt-2">
+      <div className="md:w-1/3 ml-4 bg-red-400">
+        <h3 className="text-2xl">Reading Time: {readingTime}</h3>
+      </div>
       <h2 className="text-3xl pl-4 mt-4">
         Bookmarked Blogs:{bookmarks.length}
       </h2>
@@ -16,5 +19,6 @@ const Bookmarks = ({ bookmarks }) => {
 };
 Bookmarks.propTypes = {
   bookmarks: PropTypes.array,
+  readingTime: PropTypes.number,
 };
 export default Bookmarks;
